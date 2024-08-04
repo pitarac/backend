b27dcd2ea9fd67c6024c7d40a79fbc576e758f60
Paulo Henrique Lima Leocádia <paulopitarac@gmail.com>
2024-08-04 15:31:25 -0300

    first commit

---
+++ bot.js
@@ -0,0 +1,44 @@
+const TelegramBot = require('node-telegram-bot-api');
+const dotenv = require('dotenv');
+
+// Carregar variáveis de ambiente do arquivo .env
+dotenv.config();
+
+const token = process.env.TELEGRAM_BOT_TOKEN;
+const bot = new TelegramBot(token, { polling: true });
+
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+  const firstName = msg.from.first_name;
+
+  const welcomeMessage = `
+  Olá, ${firstName}! Bem-vindo ao Airdrop da Capivara Coin!
+
+  Para participar do nosso airdrop e ganhar tokens grátis, siga as instruções abaixo:
+  1. Acesse nosso airdrop e siga as etapas necessárias.
+  2. Certifique-se de cumprir todas as condições para ser elegível.
+
+  Se precisar de ajuda, sinta-se à vontade para nos contatar. Boa sorte!
+
+  Equipe Capivara Coin 🦫
+  `;
+
+  const options = {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          { text: 'Acessar nosso airdrop', url: 'https://t.me/Capivaracoin_bot/airdrop' },
+          { text: 'Visitar o nosso site', url: 'https://capivara.online?utm_source=telegram&utm_medium=bot&utm_campaign=site' }
+        ]
+      ]
+    }
+  };
+
+  bot.sendMessage(chatId, welcomeMessage, options);
+});
+
+bot.on('message', (msg) => {
+  console.log(`Mensagem recebida de ${msg.chat.id}: ${msg.text}`);
+});
+
+console.log('Bot está rodando...');
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const axios = require('axios');
+const dotenv = require('dotenv');
+const path = require('path');
+
+// Carregar variáveis de ambiente do arquivo .env
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+// Iniciar o bot do Telegram
+require('./bot');
+
+app.get('/api/telegram/user', async (req, res) => {
+  try {
+    const token = process.env.TELEGRAM_BOT_TOKEN;
+    const response = await axios.get(`https://api.telegram.org/bot${token}/getMe`);
+    res.json(response.data.result);
+  } catch (error) {
+    console.error("Erro ao pegar dados do usuário do Telegram", error);
+    res.status(500).send('Erro ao pegar dados do usuário do Telegram');
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
